84a42c6f8c3f2fcdc81d336c7a9c0ece7aafc261
Christopher Read <Christopherread22@outlook.com>
2021-03-16 16:37:05 -0400

    almost

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+const header = document.querySelector("h1#header")
 console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = 'blue'
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+
+console.log('This is what the traveler object looks like: ', traveler)
 
 
 /***** Deliverable 4 *****/
